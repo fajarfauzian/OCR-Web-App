8690731296f1f986183691f439c04ef7b5cfd173
zfosix <fajarfauzian53@gmail.com>
2025-06-04 17:27:15 +0700

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <div className="text-center mt-8 text-gray-500 text-sm">
-      <p>Powered by Fajar Fauzian - OCR Engine</p>
+      <p>Powered by Tesseract.js - OCR Engine</p>
     </div>
   );
 };
